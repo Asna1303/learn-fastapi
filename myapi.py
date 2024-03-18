f77882677cc6be5196cf590faf46723aa0322450
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-03-18 23:11:07 +0530

    added more path componnets

--- myapi.py
+++ myapi.py
@@ -1,16 +1,22 @@
+
+
 from fastapi import FastAPI, Path
-app=FastAPI()
-students={
-    1:{
-        "name":"john",
-        "age":17,
-        "class":"year 12"
+
+app = FastAPI()
+
+students = {
+    1: {
+        "name": "john",
+        "age": 17,
+        "class": "year 12"
     }
 }
+
 @app.get("/")
 def index():
-    return{"name":"First Data"}
+    return {"name": "First Data"}
 
 @app.get("/get-students/{student_id}")
-def get_student(student_id: int):
-    return students[student_id]
+def get_student(student_id: int = Path(..., description="the id of the student you want to view", gt=0,lt=3)):
+  
+     return students[student_id]
